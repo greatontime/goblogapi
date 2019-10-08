934da6890879c2719738364c288a659f330600ae
greatontime <pathomphong1144@gmail.com>
2019-10-09 00:51:38 +0700

    Add Handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -15,3 +15,16 @@ func GetAllBlog(w http.ResponseWriter, r *http.Request){
 	payload := dao.GetAllBlog()
 	json.NewEncoder(w).Encode(payload)
 }
+
+//CreateBlog create blog
+func CreateBlog(w http.ResponWriter, r *http.Request){
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	var blog models.Blog
+	_ = json.NewDecoder(r.Body).Decode(&blog)
+	// fmt.Println(task, r.Body)
+	dao.InsertOneBlog(blog)
+	json.NewEncoder(w).Encode(blog)
+}
