86a99e47c0ad72d27febd8d9aeca3025ea4fdda8
greatontime <pathomphong1144@gmail.com>
2019-10-09 01:26:24 +0700

    Add Rounter delete

--- router/router.go
+++ router/router.go
@@ -14,6 +14,8 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/blog",handlers.CreateBlog).Methods("POST","OPTIONS")
 	router.HandleFunc("/api/blog/{id}", handlers.ActiveBlog).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/unactiveblog/{id}", handlers.UnActiveBlog).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deleteblog/{id}", handlers.DeleteBlog).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/deleteallblog", handlers.DeleteAllBlog).Methods("DELETE", "OPTIONS")
 
 	return router
 }
